fafabdd8599bbc08d6440bdf78a32d79daf8a08e
Firaol Bogale <firaolbogala98@gmail.com>
2025-05-20 08:18:06 +0300

    refactor: reorganize search endpoint and improve document loading process

--- src/api/endpoints/search.py
+++ src/api/endpoints/search.py
@@ -1,42 +1,42 @@
 from fastapi import APIRouter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from pydantic import BaseModel
 from langchain_core.runnables import chain
-
 
 router = APIRouter()
 
+class SearchInput(BaseModel):
+  question: list[str]
+
 filepath = "src\docs\document.pdf"
-loader = PyPDFLoader(file_path=filepath)
+loader = PyPDFLoader(filepath)
 docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
-all_splits = text_splitter.split_documents(docs)
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True)
+
+all_split = splitter.split_documents(docs)
 
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
 vector_store = Chroma(
-    collection_name="example_collection",
-    embedding_function=embeddings,
-    persist_directory="src/chroma_langchain_db", 
+  collection_name="example_collection",
+  embedding_function=embeddings,
+  persist_directory="src/another_chroma"
 )
 
-vector_store.add_documents(documents=all_splits)
-
-results = vector_store.similarity_search(
-    "what is compiler?"
-)
+vector_store.add_documents(documents=all_split)
 
 @chain
 def retriever(query):
   return vector_store.similarity_search(query, k=1)
 
-batch_retrieve = retriever.batch([ "what is compiler?", "why steps are there in compiler?"])
-
-
-@router.get('/search')
-def getDocLen():
-  return len(all_splits), batch_retrieve
-
+@router.post('/')
+def search(search:SearchInput):
+  questions = search.question
+  
+  result = retriever.batch(questions)
+  
+  return {"question":questions,"result" : result}
